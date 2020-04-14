469daa141c5ec009a5f34314d21495f072ab99b5
Uniffay <56744682+Uniffay@users.noreply.github.com>
2020-04-14 12:34:17 +0200

    wesh on a tro avancé

---
+++ src/formulaire.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Connectez-vous !</title>
+		<link rel="stylesheet" type="text/css" href="accueil.css">
+	</head>
+	<body>
+		<form class="enPleinMilieu">
+			<div class="centrer">
+				<div class="space">
+					<label for="Username" >Username:</label>
+					<input type="text" name="username" id="Username">
+				</div>
+			</div>
+			<div class="centrer">
+				<div class="space">
+					<label for="Password">Password:</label>
+					<input type="password" name="password" id="Password">
+				</div>
+			</div>
+			<button type="submit" class="buttonTaille">Se Connecter</button>
+		</form>
+	</body>
+</html>
