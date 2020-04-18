8eecbd14af67a7c1fdb63cb05a826f6611216681
Uniffay <56744682+Uniffay@users.noreply.github.com>
2020-04-18 13:28:12 +0200

    cc trop bien

--- src/formulaire.php
+++ src/formulaire.php
@@ -1,3 +1,11 @@
+<?php
+if (isset($_POST["password"]) && isset($_POST["username"])) {
+	
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,20 +13,21 @@
 		<link rel="stylesheet" type="text/css" href="accueil.css">
 	</head>
 	<body>
-		<form class="enPleinMilieu">
+		<form class="enPleinMilieu" method="POST">
 			<div class="centrer">
 				<div class="space">
 					<label for="Username" >Username:</label>
-					<input type="text" name="username" id="Username">
+					<input type="text" name="username" id="Username" size="25" required>
 				</div>
 			</div>
 			<div class="centrer">
 				<div class="space">
 					<label for="Password">Password:</label>
-					<input type="password" name="password" id="Password">
+					<input type="password" name="password" id="Password" size="25" required>
 				</div>
 			</div>
 			<button type="submit" class="buttonTaille">Se Connecter</button>
 		</form>
 	</body>
 </html>
+
