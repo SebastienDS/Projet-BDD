e9494690e28ced61473d6cfdfffa8572328afb94
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-05-09 11:18:28 +0200

    supprime le jardinier des Residences

--- supprimer_un_compte.php
+++ supprimer_un_compte.php
@@ -4,13 +4,21 @@ require_once("connexion.php");
 
 if (isset($_GET["id"])) {
     $id = (int)$_GET["id"];
-    $stmt_jardinier = $dbh->prepare("DELETE from Jardinier where num_individu = ?");  //jardinier first (clé etrangere)
+
+    $dbh->exec("set foreign_key_checks = 0");
+
+    $stmt_jardinier = $dbh->prepare("DELETE from Jardinier where num_individu = ?");
     $stmt_individu = $dbh->prepare("DELETE from Individu where num_individu = ?");
     $stmt_authentification = $dbh->prepare("DELETE from authentification where id = ?");
+    $stmt_residence = $dbh->prepare("DELETE from Residence where num_individu = ?");
 
     $stmt_jardinier->execute([$id]);
     $stmt_individu->execute([$id]);
     $stmt_authentification->execute([$id]);
+    $stmt_residence->execute([$id]);
+
+    $dbh->exec("set foreign_key_checks = 1");
+
 }
 
 header("location: gestion_des_comptes.php");
