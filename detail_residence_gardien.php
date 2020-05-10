97b03a975e512d6a2353cc424aea9a1cb62cd721
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-05-10 09:33:24 +0200

    information gardien lié a la residence

--- detail_residence_gardien.php
+++ detail_residence_gardien.php
@@ -1,1 +1,21 @@
-<p>gardien</p>
+<?php
+require_once('connexion.php');
+$stmt = $dbh->prepare("SELECT num_individu as 'Numéro', nom_individu as 'Nom', prenom_individu as 'Prénom', 
+    adresse_individu as 'Adresse', telephone_individu as 'Numéro de téléphone', 
+    e_mail_gardien as 'Adresse email' 
+    from Individu natural join Gardien where num_residence = ?");
+$stmt->execute([$_GET['id']]);
+$jardinier = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<?php foreach ($jardinier as $elem) { ?>
+    <div class="fullWidth presentationJardinier" id="<?= $elem['Numéro'] ?>">
+        <img src="img/concierge.png" width="200" class="espace">
+        <div class="contenuJardinier espace">
+            <?php foreach ($elem as $key => $value) { ?>
+                <p class="itemDescription"><?= $key ?> : <?= $value ?></p>
+            <?php } ?>
+        </div>
+    </div>
+<?php } ?>
